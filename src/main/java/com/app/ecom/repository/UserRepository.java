341db66feae6c5f422f7f5ea414001b4e4121f1a
Suraj-codes1410 <samantasuraj1410@gmail.com>
2026-06-12 19:01:25 +0530

    docker yaml

---
+++ src/main/java/com/app/ecom/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.app.ecom.repository;
+
+import com.app.ecom.model.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<AppUser,Long> {
+
+}
