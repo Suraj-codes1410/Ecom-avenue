cfd18416586ce147120966d9711f0918e177fe1c
Suraj-codes1410 <samantasuraj1410@gmail.com>
2026-06-13 16:24:32 +0530

    Fix PostgreSQL timezone issue

--- src/main/java/com/app/ecom/EcomApplication.java
+++ src/main/java/com/app/ecom/EcomApplication.java
@@ -3,11 +3,17 @@ package com.app.ecom;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
-public class   EcomApplication {
+public class EcomApplication {
 
 	public static void main(String[] args) {
+
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+
+		System.out.println("TIMEZONE = " + TimeZone.getDefault().getID());
+
 		SpringApplication.run(EcomApplication.class, args);
 	}
-
 }
